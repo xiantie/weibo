de7fa9eb19d700d438f673da2b5820d9c53ad908
tie <303291556@qq.com>
2021-03-26 06:22:08 +0000

    访问限流

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -12,6 +12,12 @@ use Carbon\Carbon;
 
 class PasswordController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('throttle:3,10', [
+            'only' => ['sendResetLinkEmail']
+        ]);
+    }
     public function showLinkRequestForm()
     {
         return view('auth.passwords.email');
